42461a1ed0837f6e51776c374b2e449120c633d8
AngelJTumG <javierangel43.lo@gmail.com>
2025-02-13 20:22:43 -0600

    add:  login y register

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,64 @@
+import { hash, verify } from "argon2";
+import Cliente from "../cliente/cliente.model.js";
+import { generateJWT } from "../helpers/generate-jwt.js";
+
+export const register = async (req, res) => {
+    try {
+        const data = req.body;
+        let profilePicture = req.file ? req.file.filename : null;
+        const encryptedPassword = await hash(data.password);
+        data.password = encryptedPassword;
+        data.profilePicture = profilePicture;
+
+        const cliente = await Cliente.create(data);
+
+        return res.status(201).json({
+            message: "Cliente ha sido creado",
+            nombre: cliente.nombre,
+            email: cliente.email
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Registro de cliente fallido",
+            error: err.message
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const cliente = await Cliente.findOne({ email: email });
+
+        if (!cliente) {
+            return res.status(400).json({
+                message: "Credenciales inválidas",
+                error: "No existe el cliente con el correo ingresado"
+            });
+        }
+
+        const validPassword = await verify(cliente.password, password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                message: "Credenciales inválidas",
+                error: "Contraseña incorrecta"
+            });
+        }
+
+        const token = await generateJWT(cliente.id);
+
+        return res.status(200).json({
+            message: "Inicio de sesión exitoso",
+            userDetails: {
+                token: token,
+                profilePicture: cliente.profilePicture
+            }
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Inicio de sesión fallido, error del servidor",
+            error: err.message
+        });
+    }
+};
